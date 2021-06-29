172c5f193f6f64a0176e050265ed6cd21b93a1bf
HieuNF <minhhieudn98@gmail.com>
2021-06-29 21:48:09 +0700

    fix light button

--- js/lora.js
+++ js/lora.js
@@ -24,7 +24,6 @@ var humidityArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var airQualityArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var dustDensityArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var timeArr = ['00:00:00', '00:00:00', '00:00:00','00:00:00', '00:00:00', '00:00:00', '00:00:00', '00:00:00','00:00:00', '00:00:00'];
-var i = 0;
 
 //********************************************
 //handle logic of LORA web table
@@ -77,7 +76,7 @@ var convertData = (deviceData) => {
     timeArr.unshift(convertTimeShort(samplingTime));
     timeArr.pop();
 
-    if (i > 1)
+    if (dataTableArr.length > 0)
       createChart();
 
     //format date for table
@@ -104,37 +103,9 @@ dbref.on('value', deviceObj => convertData(deviceObj.val()));
 //********************************************
 //handle logic of LORA web chart
 //********************************************
-
-//var temperatureLimit = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100'];
-
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
-
-// function number_format(number, decimals, dec_point, thousands_sep) {
-//   // *     example: number_format(1234.56, 2, ',', ' ');
-//   // *     return: '1 234,56'
-//   number = (number + '').replace(',', '').replace(' ', '');
-//   var n = !isFinite(+number) ? 0 : +number,
-//     prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-//     sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-//     dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-//     s = '',
-//     toFixedFix = function(n, prec) {
-//       var k = Math.pow(10, prec);
-//       return '' + Math.round(n * k) / k;
-//     };
-//   // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-//   s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-//   if (s[0].length > 3) {
-//     s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-//   }
-//   if ((s[1] || '').length < prec) {
-//     s[1] = s[1] || '';
-//     s[1] += new Array(prec - s[1].length + 1).join('0');
-//   }
-//   return s.join(dec);
-// }
 
 var ctxT = document.getElementById("myAreaChartTemperatue");
 var ctxH = document.getElementById("myAreaChartHumidity");
@@ -493,107 +464,59 @@ var createChart = () => {
 }
 createChart ();
 
-var chartTest = () => {
-  Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: timeArr,
-      datasets: [{
-        label: "Earnings",
-        lineTension: 0.3,
-        backgroundColor: "rgba(78, 115, 223, 0.05)",
-        borderColor: "rgba(78, 115, 223, 1)",
-        pointRadius: 3,
-        pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointBorderColor: "rgba(78, 115, 223, 1)",
-        pointHoverRadius: 3,
-        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        pointHitRadius: 10,
-        pointBorderWidth: 2,
-        data: temperatureArr,
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      layout: {
-        padding: {
-          left: 10,
-          right: 25,
-          top: 25,
-          bottom: 0
-        }
-      },
-      scales: {
-        xAxes: [{
-          time: {
-            unit: 'date'
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          ticks: {
-            maxTicksLimit: 7
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            maxTicksLimit: 5,
-            padding: 10,
-            // Include a dollar sign in the ticks
-            callback: function (value, index, values) {
-              return value + '°C';
-            }
-          },
-          gridLines: {
-            color: "rgb(234, 236, 244)",
-            zeroLineColor: "rgb(234, 236, 244)",
-            drawBorder: false,
-            borderDash: [2],
-            zeroLineBorderDash: [2]
-          }
-        }],
-      },
-      legend: {
-        display: false
-      },
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        titleMarginBottom: 10,
-        titleFontColor: '#6e707e',
-        titleFontSize: 14,
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        intersect: false,
-        mode: 'index',
-        caretPadding: 10,
-        callbacks: {
-          label: function (tooltipItem, chart) {
-            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-            return datasetLabel + ': ' + tooltipItem.yLabel + '°C';
-          }
-        }
-      }
-    }
-  });
-};
-
 //********************************************
 //handle logic of LORA web button
 //********************************************
+
+var checkFan = () => {
+  if ($('#fanText').text() == 'On')
+    return 1;
+  return 0;
+}
+
+var checkLight = () => {
+  if ($('#lightText').text() == 'On')
+    return 1;
+  return 0;
+}
+
+var dataFan = (status) => {
+  return {
+    "dev_id": "b06b01e6e2e0",
+    "port": 1,
+    "confirmed": false,
+    "payload_fields": {
+      "fan": status,
+      "light": checkLight()
+    }
+  }
+}
+
+var dataLight = (status) => {
+  return {
+    "dev_id": "b06b01e6e2e0",
+    "port": 1,
+    "confirmed": false,
+    "payload_fields": {
+      "fan": checkFan(),
+      "light": status
+    }
+  }
+}
+
+//var turnOnFan
+
+
 
 $("#fanBtn").bind( "click", () => {
   if($('#fanText').text() == 'On'){
     $('#fanText').text('Off');
     $("#fanBtn").removeClass('btn-primary').addClass('btn-danger');
+    sendDownlink(dataFan(0));
   }else {
     $('#fanText').text('On');
     $("#fanBtn").removeClass('btn-danger').addClass('btn-primary');
+    sendDownlink(dataFan(1));
   }
 });
 
@@ -607,3 +530,11 @@ $( "#lightBtn" ).bind( "click", function() {
   }
 });
 
+var sendDownlink = (data) => {
+  fetch("https://integrations.thethingsnetwork.org/ttn-asia-se/api/v2/down/esp32iotlora/lora?key=ttn-account-v2.NVbk-rHNOrWv6VW-KNrH8u2r4ymoAk8RI1nCBByEQg4", {
+    method: "POST",
+    body: JSON.stringify(data)
+  }).then(res => {
+    console.log("Request complete! response:", res);
+  });
+}
